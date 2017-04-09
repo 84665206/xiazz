7d20f9482c149fdd3109e4ab3077280f86238cc7
84665206 <84665206@qq.com>
2017-04-09 17:21:01 +0800

    初始导入

---
+++ HelloWorld/src/com/lw/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.lw;
+
+
+/**
+ * <p>
+ * TODO类概述
+ * </p>
+ * <p>
+ * 提供如下功能：<br>
+ * <1>.TODO<br>
+ * <2>.TODO<br>
+ * </p>
+ * @since 2017年4月9日
+ * @version V1.0
+ * @author luowen
+ */
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello,World!");
+		System.out.println("Hello,World-2!");
+		System.out.println("Hello,World-3!");
+	}
+}
